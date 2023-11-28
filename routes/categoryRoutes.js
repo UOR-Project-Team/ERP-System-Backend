8f820072b9de01ba28dfd101736d3bd7d786eecb
Jeewanthaherath <jeewanthaherath98@gmail.com>
2023-11-28 18:12:12 +0530

    added category Routes JS file

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,27 +1,7 @@
 const express = require('express')
-const route = express.Router()
-const mysql= require('mysql2');
+const router = express.Router()
+const categoryController = require('../controllers/categoryController');
 
+router.post('/create', categoryController.createCategory);
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'mysqluserpwd',
-//     database: 'add_category'
-//   });
-
-//  route.post("/", (req , res)=>{
-//      const sql = "INSERT INTO category (Name,Description) VALUES (?)"
-//      const values=  [
-//         req.body.Name,
-//         req.body.Description
-//      ]
-     
-
-//      connection.query(sql,[values],(err,data) =>{
-//         if(err) return res.json('error');
-//         return res.json(data);
-//      })
-//  })
-
- module.exports = route
+ module.exports = router;
