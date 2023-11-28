f4f812c4ce4ee44283491cdbe6f2804948accb73
Jeewantha Herath <jeewanthah@Jeewanthas-MacBook-Air.local>
2023-11-28 07:47:11 +0530

    initial commit Category Master

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
+app.use('/category', categoryRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
