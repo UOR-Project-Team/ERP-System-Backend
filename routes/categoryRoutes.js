beea69199fb859558f1aa57f328a317e542a8cb7
Jeewanthaherath <jeewanthaherath98@gmail.com>
2023-12-16 13:10:33 +0530

    Category update and delete backend implemented

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,9 +5,8 @@ const categoryModel= require('../models/categoryModel');
 
 router.post('/create', categoryController.createCategory);
 router.get('/show', categoryController.showCategory);
-
 router.delete('/delete/:id', categoryController.deleteCategory);
-
-
+router.get('/show/:id',categoryController.getCategoryById);
+router.put('/update/:id', categoryController.updateCategory);
 
  module.exports = router;
