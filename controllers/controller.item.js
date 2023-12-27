a8c5d6dc457947149d4d20140d428c01aaa495cf
Fahim <fahimbm105@gmail.com>
2023-12-28 01:44:00 +0530

    cleaned item module backend code

--- controllers/controller.item.js
+++ controllers/controller.item.js
@@ -1,22 +1,5 @@
 const ItemModel = require('../models/itemModel');
 
-// const Additem = (req, res) => {
-//   const { code, itemName, categoryId, unitId } = req.body;
-//    // console.log("hello");
-//   const itemData = { code, itemName, categoryId, unitId };
-
-//   ItemModel.addItem(itemData, (err, results) => {
-//     if (err) {
-//       console.error('Error Adding Item:', err);
-//       return res.status(500).json({ error: 'Error Adding Item' });
-//     }else if(results && results.insertId){
-//       res.status(201).json({ message: 'Item Added Successfully', id: results.insertId });
-//     }else{
-//       return res.status(500).json({ error: 'Internal Server Error222' });
-//     }
-    
-//   });
-// };
 
 const Additem = async (req, res) => {
 
@@ -33,25 +16,6 @@ const Additem = async (req, res) => {
 
 };
 
-
-// const deleteItem = async (req, res) => {
-//   const itemId = req.params.id;
-//   console.log("delete request has reached item controller");
-//   await ItemModel.deleteItem(itemId, (err, results) => {
-//     if (err) {
-//       console.error('Error deleting item:', err);
-//       return res.status(500).json({ error: 'Error deleting item' });
-//     }
-    
-//     if (results.affectedRows > 0) {
-//       res.status(200).json({ message: 'Item deleted successfully' });
-      
-//     } else {
-      
-//       res.status(404).json({ message: 'Item not found' });
-//     }
-//   });
-// };
 
 
 const deleteItem = async (req, res) => {
@@ -71,24 +35,6 @@ const deleteItem = async (req, res) => {
 
 };
 
-// const updateItem = (req, res) => {
-//   const itemId = req.params.id
-//   const { code, itemName, categoryId, unitId } = req.body;
-  
-//   const itemData = { code, itemName, categoryId, unitId };
-
-//   ItemModel.updateItem(itemData, itemId, (err, results) => {
-//     if (err) {
-//       console.error('Error Updating Item:', err);
-//       return res.status(500).json({ error: 'Error Updating Item' });
-//     }else if(results && results.insertId){
-//       res.status(201).json({ message: 'Item Updated Successfully', id: results.insertId });
-//     }else{
-//       return res.status(500).json({ error: 'Internal Server Error222' });
-//     }
-    
-//   });
-// };
 
 const updateItem = async (req, res) => {
   try{
--- models/itemModel.js
+++ models/itemModel.js
@@ -1,21 +1,6 @@
-const db2 = require('../connection');
-const db = require('../dbConfig.js');
+const db = require('../connection');
+const db2 = require('../dbConfig.js');
 
-// const addItem = (itemData, callback) => {
-//     const { code, itemName, categoryId, unitId } = itemData;
-  
-//     const query = 'INSERT INTO product (Code, Name, Category_ID,Unit_ID) VALUES (?,?,?,?)';
-//     const values = [
-//       code, itemName, categoryId, unitId
-//     ]
-  
-//     db.query(query, values, (err, results) => {
-//       if (err) {
-//         return callback(err, null);
-//       }
-//       return callback(null, results);
-//     });
-//   };
 
   const addItem = async (itemData) => {
     try{
@@ -23,7 +8,7 @@ const db = require('../dbConfig.js');
   
       const query = 'INSERT INTO product (Code, Name, Category_ID,Unit_ID) VALUES (?,?,?,?)';
       const values = [ code, itemName, categoryId, unitId]
-      const [results]= await db2.execute(query,values);
+      const [results]= await db.execute(query,values);
 
       return results.insertId;
     } catch (err){
@@ -33,25 +18,10 @@ const db = require('../dbConfig.js');
   };
 
 
-  
-  // const deleteItem = async (itemId, callback) => {
-  //   const sql = 'DELETE FROM product WHERE ID = ?';
-  //   console.log("delete request has reached item model");
-  //   await db.query(sql, [itemId], (err, results) => {
-    
-      
-  //     if (err) {
-  //       console.error('Error deleting item:', err);
-  //       return callback(err, null);
-  //     }
-  //     return callback(null, results);
-  //   });
-  // };
-
   const deleteItem = async (itemId) => {
     try{
       const query = 'DELETE FROM product WHERE ID = ?';
-      await db2.query(query,[itemId]);
+      await db.query(query,[itemId]);
     }
     catch(err)
     {
@@ -61,29 +31,13 @@ const db = require('../dbConfig.js');
   };
   
 
-  // const updateItem = (itemData,itemId, callback) => {
-  //   const { code, itemName, categoryId, unitId } = itemData;
-  
-  //   const query = 'UPDATE product SET Code=?, Name=?, Category_ID=?,Unit_ID=? WHERE ID = ?';
-  //   const values = [
-  //     code, itemName, categoryId, unitId, itemId 
-  //   ]
-  
-  //   db.query(query, values, (err, results) => {
-  //     if (err) {
-  //       return callback(err, null);
-  //     }
-  //     return callback(null, results);
-  //   });
-  // };
-
   const updateItem = async (itemData,itemId) => {
 
     try{
       const { code, itemName, categoryId, unitId } = itemData;
       const query = 'UPDATE product SET Code=?, Name=?, Category_ID=?,Unit_ID=? WHERE ID = ?';
       const values = [ code, itemName, categoryId, unitId, itemId ]
-      await db2.query(query, values)
+      await db.query(query, values)
 
     }
     catch(err){
@@ -106,7 +60,7 @@ const getAllItems =  async () => {
                 ORDER BY
                     product.code;
                 `
-    const [results] = await db2.execute(sql);
+    const [results] = await db.execute(sql);
     
     return results;
   } catch (err) {
