ada7de37879e20c203e4216fd88d76f38c0b893f
Riyas <riyas.afm@gmail.com>
2024-01-05 14:44:39 +0530

    db update

--- controllers/controller.login.js
+++ controllers/controller.login.js
@@ -1,7 +1,7 @@
 const LoginModel = require('../models/model.login');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const {hashPassword} = require('./PasswordController');
+const {hashPassword} = require('./controller.password');
 
 const loginUser = async (req, res) => {
 
