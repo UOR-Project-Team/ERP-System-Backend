9f46388317156d577e364f1bebe98011622b870d
NuvinduSenarathne <nuvindusanjana8@gmail.com>
2024-01-10 09:21:17 +0530

    item-supplier based Item retrieve backend updates

--- routes/route.item.js
+++ routes/route.item.js
@@ -7,6 +7,7 @@ router.post('/create', itemController.Additem);
 router.get('/',itemController.getAllItems);
 router.get('/unit/:id',itemController.getAllItemsByUnit);
 router.get('/category/:id',itemController.getAllItemsByCategory);
+router.get('/supplier/:id',itemController.getAllItemsBySupplier);
 router.delete('/:id', itemController.deleteItem);
 router.put('/:id', itemController.updateItem);
 
