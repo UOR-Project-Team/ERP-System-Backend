7f35d0538dc9a9a9c728f0dc2e8ec0eed7171055
Riyas <riyas.afm@gmail.com>
2024-01-31 23:25:15 +0530

    report

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 
-// Load environment variables from .env file
-require('dotenv').config();
-
-const app = express();
-
-// Middleware
-app.use(cors());
-
-// Routes
 const userroute = require('./routes/Router.User')
 const categoryRoutes = require('./routes/route.category');
 const loginRoute = require('./routes/route.login');
@@ -19,7 +11,27 @@ const unitRoute = require('./routes/route.unit');
 const customerRoute = require('./routes/route.customer');
 const grnRoute = require('./routes/route.grn');
 const dashboardRoute = require('./routes/route.dashboard');
-const invoiceRoute = require('./routes/route.invoice');
+const invoiceRoute = require('./routes/route.invoice')
+const reportRoute = require('./routes/route.report');
+
+require('dotenv').config();
+const app = express();
+app.use(cors());
+
+
+const crypto = require('crypto');
+const randomSessionSecret = crypto.randomBytes(32).toString('hex');
+console.log(randomSessionSecret);
+
+
+app.use(
+  session({
+    secret: randomSessionSecret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 
 app.use(express.json());
 app.use('/',loginRoute)
@@ -32,6 +44,7 @@ app.use('/unit', unitRoute);
 app.use('/grn', grnRoute);
 app.use('/dashboard', dashboardRoute);
 app.use('/invoice', invoiceRoute);
+app.use('/report', reportRoute)
 
 // Configuration
 const port = process.env.PORT || 8081;
